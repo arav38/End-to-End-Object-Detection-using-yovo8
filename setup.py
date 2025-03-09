65f67dc8c463f75cb7bcfa3e12ae7b0c3589739a
unknown <arav38>
2025-03-09 22:56:09 +0530

    updatecode

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="damageDetection",
+    version="1.0.0",
+    author="Akshay Vilayatkar",
+    author_email= "arav38vilayatkar@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
